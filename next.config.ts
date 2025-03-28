af115739f60bb5e2bc3349b336ca2481ea898f3e
manoj mandal <hackintown99@gmail.com>
2025-03-28 10:26:01 +0530

    test1

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -16,6 +17,13 @@ const nextConfig: NextConfig = {
         pathname: "**",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./src"),
+    };
+    return config;
   },
 };
 
